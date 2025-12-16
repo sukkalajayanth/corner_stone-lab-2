b4c591e452d6da3ebfc5d4fd5c599e13437b2ce0
sukkalajayanth <sukkala.jayanth@gmail.com>
2025-12-16 14:09:53 +0530

    Add process creation and ptrace attach

--- debugger.c
+++ debugger.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
 
+/*
+ * Core debugger: process creation and ptrace attach
+ */
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -8,9 +14,20 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("Minimal debugger skeleton\n");
-    printf("Target program: %s\n", argv[1]);
-    printf("Debugger framework initialized.\n");
+    pid_t child = fork();
+
+    if (child == 0) {
+        // Child process (debuggee)
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execvp(argv[1], &argv[1]);
+        perror("execvp");
+        exit(1);
+    } else {
+        // Parent process (debugger)
+        int status;
+        waitpid(child, &status, 0);
+        printf("Debugger attached to process %d\n", child);
+    }
 
     return 0;
 }
